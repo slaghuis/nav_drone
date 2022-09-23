bdcdfde4f55ca743360308afd620eacfef392275
Eric Slaghuis <eslaghuis@gmail.com>
2022-09-23 10:27:37 +0200

    Add files via upload

---
+++ nav_drone_pid_controller/include/nav_drone_pid_controller/pid_controller.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+#include "nav_drone_core/controller.hpp"
+#include "nav_drone_pid_controller/histogram.hpp"
+#include "nav_drone_pid_controller/pid.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+//#include "nav_drone_util/odometry_utils.hpp"
+//#include "nav_drone_util/geometry_utils.hpp"
+
+namespace nav_drone_pid_controller
+{
+class PIDController : public nav_drone_core::Controller
+{
+  public:
+  
+    PIDController() = default;
+    ~PIDController() override = default;
+  
+    void configure(
+      const rclcpp::Node::SharedPtr node, 
+      std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+      std::shared_ptr<octomap::OcTree> costmap ) override;
+    
+    void setPath(const nav_msgs::msg::Path & path) override;
+    void updateMap(std::shared_ptr<octomap::OcTree> costmap) override;
+    
+    geometry_msgs::msg::TwistStamped computeVelocityCommands(
+      const geometry_msgs::msg::PoseStamped & pose,
+      const geometry_msgs::msg::Twist & speed) override;   
+        
+//        nav2_core::GoalChecker * goal_checker) = 0;
+  protected:
+    rclcpp::Node::SharedPtr node_;
+    std::string plugin_name_;
+    std::shared_ptr<tf2_ros::Buffer> tf_;
+    std::shared_ptr<octomap::OcTree> costmap_;
+    rclcpp::Logger logger_ {rclcpp::get_logger("PIDController")};
+    rclcpp::Clock::SharedPtr clock_;
+  
+    nav_msgs::msg::Path global_plan_;
+      
+    double lookahead_dist_;
+    double min_lookahead_dist_;
+    double max_lookahead_dist_;
+    double lookahead_time_;
+    bool use_velocity_scaled_lookahead_dist_;
+    double robot_radius_;
+    double safety_radius_;
+    
+    double desired_linear_vel_;
+    double desired_angular_vel_;
+    
+    double freq_;
+  private:  
+    double last_e_angle_, last_z_angle_;
+    
+    // PID Controllers  
+    std::shared_ptr<PID> pid_x;
+    std::shared_ptr<PID> pid_y;
+    std::shared_ptr<PID> pid_z;
+    std::shared_ptr<PID> pid_yaw;
+        
+    double getLookAheadDistance(const geometry_msgs::msg::Twist & speed);
+    std::pair<int, int> get_ez_grid_pos(const octomap:: point3d & goal);
+    std::pair<double, double> get_ez(const geometry_msgs::msg::PoseStamped & current_pose,
+                                     const geometry_msgs::msg::PoseStamped & target_pose);
+    geometry_msgs::msg::PoseStamped getLookAheadPoint(const double & lookahead_dist,
+                                                      const geometry_msgs::msg::PoseStamped & current_pose);
+
+};
+  
+}  // namespace  nav_drone_pid_controller
---
+++ nav_drone_pid_controller/src/histogram.cpp
@@ -0,0 +1,84 @@
+#include "nav_drone_pid_controller/histogram.hpp"
+#include <stdexcept>
+
+namespace nav_drone_pid_controller {
+Histogram::Histogram(const int res)
+  : resolution_{res}, z_dim_{360 / resolution_}, e_dim_{180 / resolution_}, weight_(e_dim_, z_dim_) {
+  set_zero();
+}
+
+void Histogram::upsample() {
+  if (resolution_ != ALPHA_RES * 2) {
+    throw std::logic_error(
+      "Invalid use of function upsample(). This function can only be used on a half resolution histogram.");
+  }
+  resolution_ = resolution_ / 2;
+  z_dim_ = 2 * z_dim_;
+  e_dim_ = 2 * e_dim_;
+  Eigen::MatrixXf temp_dist(e_dim_, z_dim_);
+
+  for (int i = 0; i < e_dim_; ++i) {
+    for (int j = 0; j < z_dim_; ++j) {
+      int i_lowres = floor(i / 2);
+      int j_lowres = floor(j / 2);
+      temp_dist(i, j) = weight_(i_lowres, j_lowres);
+    }
+  }
+  weight_ = temp_dist;
+}
+
+void Histogram::downsample() {
+  if (resolution_ != ALPHA_RES) {
+    throw std::logic_error(
+        "Invalid use of function downsample(). This function can only be used on a full resolution histogram.");
+  }
+  resolution_ = 2 * resolution_;
+  z_dim_ = z_dim_ / 2;
+  e_dim_ = e_dim_ / 2;
+  Eigen::MatrixXf temp_dist(e_dim_, z_dim_);
+
+  for (int i = 0; i < e_dim_; ++i) {
+    for (int j = 0; j < z_dim_; ++j) {
+      int i_high_res = 2 * i;
+      int j_high_res = 2 * j;
+      temp_dist(i, j) = weight_.block(i_high_res, j_high_res, 2, 2).mean();
+    }
+  }
+  weight_ = temp_dist;
+}
+
+void Histogram::set_zero() { weight_.fill(0.f); }
+
+bool Histogram::is_empty() const {
+  int counter = 0;
+  for (int e = 0; (e < e_dim_) && (0 == counter); e++) {
+    for (int z = 0; (z < z_dim_) && (0 == counter); z++) {
+      if (weight_(e, z) > FLT_MIN) {
+        counter++;
+      }
+    }
+  }
+  return counter == 0;
+}
+  
+void Histogram::go_binary(float theta_low, float theta_high) {  
+  
+  for (int e = 0; (e < e_dim_); e++) {
+    for (int z = 0; (z < z_dim_); z++) {
+      if (weight_(e, z) > theta_high) {
+        set_weight(e, z, 1.0);
+      } else if (weight_(e, z) < theta_low) {
+        set_weight(e, z, 0.0);
+      } else if (z > 0) {   // Avoid accessing invalid z index
+        set_weight(e, z, weight_(e,z-1));
+      } else if (e > 0) {
+        set_weight(e, z, weight_(e-1, z_dim_-1));
+      } else {
+        set_weight(e, z, 1.0); // Desperate last call.  Just dont fly to e=0, z=0
+      }        
+    }
+  }
+  
+}
+
+}  // nav_drone_pid_controller
