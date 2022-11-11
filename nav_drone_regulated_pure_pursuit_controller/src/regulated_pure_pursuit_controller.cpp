35f48454b7c30cc5f5868cb70631a1cf38ca5d6e
Eric Slaghuis <eslaghuis@gmail.com>
2022-11-11 11:18:24 +0200

    Sigh

--- nav_drone_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
+++ nav_drone_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
@@ -579,11 +579,13 @@ nav_msgs::msg::Path RegulatedPurePursuitController::transformGlobalPlan(
 
   RCLCPP_INFO(logger_, "TGP - plan has %d waypoints and is %.2f long", global_plan_.poses.size(), nav_drone_util::calculate_path_length(global_plan_));  
   RCLCPP_INFO(logger_, "TGP - closest_pose_upper_bound [%.2f,%.2f] - %.2f", closest_pose_upper_bound->pose.position.x, closest_pose_upper_bound->pose.position.y, max_robot_pose_search_dist_);
-  
+  closest_pose_upper_bound = global_plan_.poses.end();
+  RCLCPP_INFO(logger_, "TGP - closest_pose_upper_bound [%.2f,%.2f] - %.2f", closest_pose_upper_bound->pose.position.x, closest_pose_upper_bound->pose.position.y, max_robot_pose_search_dist_);
+
   // First find the closest pose on the path to the robot
   // bounded by when the path turns around (if it does) so we don't get a pose from a later
   // portion of the path
-  auto transformation_begin =
+  auto transformation_begin = 
     nav_drone_util::min_by(
     global_plan_.poses.begin(), closest_pose_upper_bound,
     [&robot_pose](const geometry_msgs::msg::PoseStamped & ps) {
